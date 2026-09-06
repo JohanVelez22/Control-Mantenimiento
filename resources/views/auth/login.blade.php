38a189da58fcce6b0f06b62fac971d67f24786b0
Johan Velez <ja.velez22@ciaf.edu.co>
2026-09-06 00:50:49 -0500

    refactorizacion login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -30,7 +30,7 @@
  </div>
 
  {{-- Card --}}
- <div class="glass-card px-6 pb-8 pt-5 md:px-8 md:pb-8 md:pt-6">
+ <div class="glass-card px-6 pb-8 pt-1 md:px-8 md:pb-8 md:pt-2">
 
  <form method="POST" action="{{ route('login') }}" class="space-y-6">
  @csrf
